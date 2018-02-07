695fd8798872b7c61a170de2c12766835b367fff
Sune Nilausen <sunenilausen@gmail.com>
2018-02-07 22:07:41 +0100

    New feature. user roles and cancancan

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :workshops
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # get '/web', to: 'workshops#web'
   get '/:workshop', to: 'workshops#show'
   get '/:workshop/:lecture', to: 'lectures#show'
   
-  # get '/:name', to: 'workshops#show', as: :workshop
-  # resources :workshops
-
   root to: "workshops#web"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205150121) do
+ActiveRecord::Schema.define(version: 20180207203143) do
 
   create_table "lectures", force: :cascade do |t|
     t.integer "number"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20180205150121) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "parent", default: false
+    t.boolean "volunteer", default: false
+    t.boolean "student", default: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
