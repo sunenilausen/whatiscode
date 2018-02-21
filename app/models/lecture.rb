651b2cbd7469bd77e7956dd6431e34d58494cf46
Sune Nilausen <sunenilausen@gmail.com>
2018-02-21 12:24:30 +0100

    New feature. ordered articles

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,7 @@
 class Lecture < ApplicationRecord
   acts_as_paranoid
 
-  has_many :article_insertions, class_name: 'ArticleLectureInsertion', inverse_of: :lecture
+  has_many :article_insertions, -> { order('number ASC') }, class_name: 'ArticleLectureInsertion', inverse_of: :lecture
   accepts_nested_attributes_for :article_insertions, reject_if: :all_blank, allow_destroy: true
 
   has_many :articles, through: :article_insertions
