767bbd259a65a850637fde250c65b492369d00db
sunenilausen <sunenilausen@gmail.com>
2018-04-14 20:47:13 +0200

    Flash and more forms

---
+++ app/assets/javascripts/flash.js
@@ -0,0 +1,9 @@
+$(document).on('turbolinks:load', function() {
+  $('.alert').append('<button class="waves-effect btn-flat close"><i class="material-icons">close</i></button>');
+
+  $('body').on('click', '.alert .close', function() {
+      $(this).parent().fadeOut(300, function() {
+          $(this).remove()
+      })
+  })
+})
