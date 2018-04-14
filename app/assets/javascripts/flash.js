9d5b7b74168795034c2813c22ac846152c0c7cc3
sunenilausen <sunenilausen@gmail.com>
2018-04-15 00:35:25 +0200

    flash and moved spaceinvader and pacman

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('.alert').append('<button class="waves-effect btn-flat close"><i class="material-icons">close</i></button>');
+  $('.alert').append('<button class="waves-effect btn-flat right close"><i class="material-icons">close</i></button>');
 
   $('body').on('click', '.alert .close', function() {
       $(this).parent().fadeOut(300, function() {
