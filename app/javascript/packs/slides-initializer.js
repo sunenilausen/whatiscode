c72d2766cf4266484e278d2aacc2b6941e03958b
sunenilausen <sunenilausen@gmail.com>
2018-07-27 14:48:28 +0200

    Remark slides initializes and uninintializes properly

--- app/javascript/packs/slides-initializer.js
+++ app/javascript/packs/slides-initializer.js
@@ -1,5 +1,8 @@
-$(document).on('turbolinks:load', function(e) {
-  if ( $( ".remark-slides" ).length ) {
-    var slideshow = remark.create({ ratio: '16:9' });
-  };
+$(document).on('turbolinks:load ready', function(e) {
+  if ( $( ".remark-slides" ).length >= 1 ) {
+    remark.create({ ratio: '16:9' });
+    $( "html.remark-container" ).each( function() {
+      $(this).removeClass();
+    })
+  }
 });
