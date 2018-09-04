3b7d77cdca9e3f2968751a520bdbb13df8923b17
sunenilausen <sunenilausen@gmail.com>
2018-09-04 23:03:55 +0200

    Project and showcase validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :showcases, -> { order(:released_at) }
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :completed_at, presence: true
 end
--- app/models/showcase.rb
+++ app/models/showcase.rb
@@ -1,3 +1,7 @@
 class Showcase < ApplicationRecord
   has_and_belongs_to_many :projects, -> { order(:created_at) }
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :released_at, presence: true
 end
