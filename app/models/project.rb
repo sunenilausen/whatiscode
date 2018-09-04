7e116eb56aa9ea6e36d2152b9d7326c769a0c88b
sunenilausen <sunenilausen@gmail.com>
2018-09-04 23:16:35 +0200

    default image url for project and showcases

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,9 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :completed_at, presence: true
+
+  def image_url
+    return self[:image_url] if self[:image_url].present?
+    ActionController::Base.helpers.image_url "hack-logo.png"
+  end
 end
--- app/models/showcase.rb
+++ app/models/showcase.rb
@@ -4,4 +4,9 @@ class Showcase < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :released_at, presence: true
+
+  def image_url
+    return self[:image_url] if self[:image_url].present?
+    ActionController::Base.helpers.image_url "hack-logo.png"
+  end
 end
