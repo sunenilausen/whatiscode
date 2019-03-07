265575cb7b465192c30820a4f343160d6e248f79
sunenilausen <sunenilausen@gmail.com>
2019-03-08 00:14:53 +0100

    also search on category title

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function(){
   $('.search-toggle').on('click', function(event) {
     $("#article_search").addClass("show");
-    $("#q_title_cont").focus();
+    $("#q_title_or_category_title_cont").focus();
   });
 
-  $("#q_title_cont").focusout(function(event) {
+  $("#q_title_or_category_title_cont").focusout(function(event) {
     $("#article_search").removeClass("show");
   });
 });
