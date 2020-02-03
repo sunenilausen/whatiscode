e2f488766d4848d9594d80ea8e3f9d14fd0fcfc2
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2020-02-03 01:37:50 +0100

    Replaced carlsberg pirates with hvaderkode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   # layout 'application', only: :htmlpage
 
   def welcome
-    @js_example ="\n ```javascript \nif (confirm('Vil du lære JavaScript?')) { \n  window.location.href = 'http://carlsbergpirates.dk/javascript'\n} else {\n  window.location.href = 'http://carlsbergpirates.dk/workshops'\n}\n```"
+    @js_example ="\n ```javascript \nif (confirm('Vil du lære JavaScript?')) { \n  window.location.href = 'http://hvaderkode.dk/javascript'\n} else {\n  window.location.href = 'http://hvaderkode.dk/workshops'\n}\n```"
 
     @css_example="\n```css\n.flyt-musen-her {\n  padding: 24px;\n  border-radius: 2px;\n  width: 70%;\n  background-color: white;\n  box-shadow: 0 2px 6px 0 rgba(0,0,0,0.4);\n}\n.flyt-musen-her:hover {\n  transition: 3s;\n  margin-left: 30%;\n}\n```"
 
--- app/javascript/packs/welcome.js
+++ app/javascript/packs/welcome.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function(e) {
   $('#run-js-welcome').click( function() {
-    if (confirm('Vil du lære JavaScript?')) { 
-      window.location.href = 'http://carlsbergpirates.dk/javascript'
+    if (confirm('Vil du lære JavaScript?')) {
+      window.location.href = 'http://hvaderkode.dk/javascript'
     } else {
-      window.location.href = 'http://carlsbergpirates.dk/workshops'
+      window.location.href = 'http://hvaderkode.dk/workshops'
     }
   })
 })
